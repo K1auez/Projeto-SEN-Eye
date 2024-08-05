649ee1634b7445d746db6d352919e7a20d33dccd
Kauê de Souza <125032282+K1auez@users.noreply.github.com>
2024-08-05 09:15:16 -0300

    Add files via upload

---
+++ SEN-Eye/index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <title>SEN-Eye</title>
+</head>
+<body>
+    <header>
+        <div class="header" id="header">
+            <div class="logo"><p>SEN-Eye</p></div>
+            <div class="links">
+                <a href="header">Início</a>
+                <a href="#sobre">Sobre</a>
+                <a href="#aplicativo">Aplicativo</a>
+                <a href="#footer">Contato</a>
+            </div>
+        <!-- <div class="btn"><a href="">sobre</a></div> -->
+        </div>
+    </header>
+
+    <div class="home" id="home">
+        <div class="home-upper-text">
+            <h1>Um passo à frente da natureza</h1>
+        </div>
+        <div class="home-description">
+            <div class="description">
+            <p>Acompanhe catástrofes naturais e análises climáticas em tempo real.</p>
+            <div class="description-btns">
+                <a class="blue-btn" href="https://www.figma.com/design/TqhktfHZFxjF23Ru75Veqr/FIGMA-SEN-EYE?node-id=0%3A1&t=W1aEpAeqgH75FEPv-1">Visualize nosso aplicativo</a>
+                <a class="gray-btn" href="">Baixe nosso aplicativo</a>
+            </div>
+            </div>
+        </div>
+        <img src="./img/planeta.png">
+    </div>
+
+    <div class="sobre" id="sobre">
+    <div class="espaco"></div>
+    <h1>Saiba mais sobre a <span class="span-logo">SEN-Eye</span></h1>
+        <div class="sobre-boxes">
+            <div class="sobre-box-row">
+            <div class="sub-box">
+            <div class="sobre-box-1"><img src="./img/cartaovisita.png"></div>
+            <div class="sobre-box-1"><img src="./img/autdor.png"></div>
+            </div>
+            <div class="sobre-box-2">
+                <h3>Marca</h3>
+                <p>Em meio a tanta tecnologia que nos rodeia, como não usar isso ao nosso favor? Por isso, a SEN-Eye vem para auxiliar a sociedade para com os desastres e tragédias, que infelizmente são inevitáveis. Como farol de esperança, a SEN-Eye utiliza a tecnologia para mitigar os efeitos dos desastres. Te damos acesso às estruturas afetadas e às áreas em risco, para que juntos possamos promover um mundo melhor!</p>
+            </div>
+            </div>
+            <div class="sobre-box-row">
+            <div class="sobre-box-2">
+            <h3>Equipe</h3>
+            <p>Marcada por desenvolvedores de software, nossa missão é evoluir o modo de como a sociedade lida com tragédias. Sabemos que, diante de desastres, toda e qualquer situação se torna mais difícil; entretanto, acreditamos que devemos zelar pelo que é nosso. Assim, propomos um recurso viável, prático e sustentável, acima de tudo, para auxiliar nas possíveis questões impeditivas à solidariedade do nosso povo.</p>
+            </div>
+            <div class="sobre-box-2"><img src="./img/empresa.jpg"></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="saibamais">
+    
+    <h1>Saiba mais sobre oque a <span class="span-logo">SEN-Eye</span> fornece</h1>
+
+        <div class="saibamais-boxes">
+                <div class="saibamais-box"><ion-icon name="partly-sunny-outline"></ion-icon><p class="list">Previsão do tempo</p></div>
+                <div class="saibamais-box"><ion-icon name="water-outline"></ion-icon><p class="list">Detectar enchentes e inundações</p></div>
+                <div class="saibamais-box"><ion-icon name="flame-outline"></ion-icon><p class="list">Localizar queimadas</p></div>
+                <div class="saibamais-box"><ion-icon name="leaf-outline"></ion-icon><p class="list">Indicar desmatamentos em áreas de presenvação</p></div>
+                <div class="saibamais-box"><ion-icon name="trending-down-outline"></ion-icon><p class="list">Reconhecer rompimentos de barragens e deslizamentos</p></div>
+                <div class="saibamais-box"><ion-icon name="warning-outline"></ion-icon><p class="list">Emitir alertas</p></div>
+                <div class="saibamais-box"><ion-icon name="hand-left-outline"></ion-icon><p class="list">Resgate de pessoas</p></div>
+                <div class="saibamais-box"><ion-icon name="man-outline"></ion-icon><p class="list">Análise de densidade demográfica</p></div>
+        </div>
+    </div>
+
+    <div class="aplicativo" id="aplicativo">
+        <div class="app-container-1">
+            <h1>Aplicativo <span class="span-logo">SEN-Eye</span></h1>
+            <div class="app-celulares">
+            <img class="cell-1" src="./img/celular-1.png">
+            <img class="cell-2" src="./img/celular-2.png">
+            </div>
+            
+        </div>
+        <div class="app-container-2">
+            <div class="app-container-2-infos">
+            <p class="app-title-bold">Com inovação tecnológica estamos dispostos a proteger vidas e preservar o meio ambiente.</p>
+            <p class="app-title-regular">Nosso aplicativo oferece suporte em tragédias naturais para minimizar seus impactos, proporcionando recursos essenciais quando mais são necessários.</p>
+            <a href="">Baixe o aplicativo <ion-icon name="download-outline"></ion-icon></a>
+            </div>
+        </div>
+    </div>
+
+    <footer id="footer">
+        <div class="footer-box">
+            <h3>Dos criadores</h3>
+            <p>Kauê de Souza</p>
+            <p>João Paulo</p>
+            <p>Mariana Ferreira</p>
+            <p>Luana Tavares</p>
+        </div>
+        <div class="footer-box">
+            <h3>Redes Socias</h3>
+            <p>Esteja sempre por dentro do que a SEN-Eye pode oferecer!</p>
+            <div class="footer-redes">
+            <a href=""><ion-icon name="logo-instagram"></ion-icon></a>
+            <a href=""><ion-icon name="logo-whatsapp"></ion-icon></a>
+            </div>
+        </div>
+        <div class="footer-box">
+            <h3>Fale conosco</h3>
+            <form method="post">
+                <div class="contato">
+                <div class="inputs">
+                <input type="text" placeholder="Nome">
+                <input type="text"placeholder="E-mail">
+                </div>
+                <div class="textarea">
+                <textarea placeholder="Assunto"></textarea>
+                </div>
+                </div>
+                <ion-icon name="send-outline"><input type="button"></ion-icon>
+            </form>
+        </div>
+        <div class="footer-box">
+            <a class="back" href="#home"><ion-icon name="arrow-up-outline"></ion-icon></a>
+        </div>
+    </footer>
+
+</body>
+</html>
